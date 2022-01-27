1da9abeca8cbe783eea2d18c40fbfcdde9952ed6
EC2 Default User <ec2-user@ip-172-31-30-63.us-east-2.compute.internal>
2022-01-27 02:24:06 +0000

    added configuration

--- configure.sh
+++ configure.sh
@@ -1,6 +1,18 @@
-ls
+cd ../cns_solve_1.3/
+make install
 
-# cd cns_solve_1.3
-# make install
-# cd ..
-# cd haddock2.4-2021-05
+unset -v latest
+for file in "/home/ec2-user/software/cns_solve_1.3/intel-x86_64bit-linux/source"/*; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+
+sed "s|^set CNSTMP=.*|set CNSTMP=${latest}|" /home/ec2-user/software/haddock2.4-2021-05/config.local 
+
+cd /home/ec2-user/software/haddock2.4-2021-05
+./install.csh config.local
+
+sed -i "s/^export.*/export HADDOCK HADDOCKTOOLS PYTHONPATH/" /home/ec2-user/software/haddock2.4-2021-05/haddock_configure.sh
+sed -i "14,17d" /home/ec2-user/software/haddock2.4-2021-05/haddock_configure.sh
+
+cd ../haddock2.4-2021-05
+source ./haddock_configure.sh
